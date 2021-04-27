bcf357f53099fe8824d561c3f3804a40f33cb529
Holsko <Mathiasholsko@hotmail.com>
2021-04-27 15:31:31 +0200

    Use onComplete / Onsuccess and OnFailure properly. Clear local Cache before internet tests

--- SmartSaleAPP/app/src/androidTest/java/mmm/i7bachelor_smartsale/app/InstrumentedTest.java
+++ SmartSaleAPP/app/src/androidTest/java/mmm/i7bachelor_smartsale/app/InstrumentedTest.java
@@ -4,39 +4,29 @@ import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
+import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
-
-import mmm.i7bachelor_smartsale.app.Models.PrivateMessage;
-import mmm.i7bachelor_smartsale.app.Models.Repository;
-import mmm.i7bachelor_smartsale.app.Models.SalesItem;
 
 import static com.google.android.gms.tasks.Tasks.await;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -70,6 +60,33 @@ public class InstrumentedTest {
         //https://stackoverflow.com/questions/47926382/how-to-configure-shorten-command-line-method-for-whole-project-in-intellij
         //https://developer.android.com/training/testing/unit-testing/local-unit-tests
         //https://stackoverflow.com/questions/2321829/android-asynctask-testing-with-android-test-framework
+
+        @Test
+        public void testConnection() throws InterruptedException {
+            final CountDownLatch signal = new CountDownLatch(1);
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            //Clear local cache of firebase elements.
+            firestore.clearPersistence();
+            firestore.collection("SalesItems").document(ouch_document_path).get()
+            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                //Use onSuccess instead of OnComplete to ensure we hit onFailure, if there is no internet
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    assertNotNull(documentSnapshot);
+                    Log.d("Test", "In onComplete");
+                    signal.countDown();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    fail();
+                    Log.d("Test", "In OnFailure");
+                    signal.countDown();
+                }
+            });
+            signal.await();
+        }
+
         @Test
         public void test_hardcoded_path_title_is_unchanged() throws InterruptedException {
             final CountDownLatch signal = new CountDownLatch(1);
